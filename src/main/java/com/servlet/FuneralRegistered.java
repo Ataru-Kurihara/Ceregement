f5648cc922e3a239f8fefb816028e7ed8ef9ef5b
ataru <ataru4101826514@gmail.com>
2022-12-05 13:17:56 +0900

    develop funnelRegister

---
+++ src/main/java/com/servlet/FuneralRegistered.java
@@ -0,0 +1,22 @@
+package com.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/FunnelRegistered")
+public class FuneralRegistered extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    public FuneralRegistered() {super();}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        getServletContext().getRequestDispatcher("/funnelRegistered.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        getServletContext().getRequestDispatcher("/funnelRegistered.jsp").forward(request, response);
+    }
+}
